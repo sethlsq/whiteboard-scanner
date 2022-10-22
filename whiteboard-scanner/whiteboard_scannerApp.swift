92fac3fb1bb658790e64e2a71a115c9715c164d5
mudd258 <81851831+mudd258@users.noreply.github.com>
2022-10-22 10:09:42 +0800

    Initial Commit

---
+++ whiteboard-scanner/whiteboard_scannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  whiteboard_scannerApp.swift
+//  whiteboard-scanner
+//
+//  Created by Seth Loh on 22/10/22.
+//
+
+import SwiftUI
+
+@main
+struct whiteboard_scannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
