07cb13c8881c3f69489da98624c0cc49d9d5efdf
sethlsq <slsq258@outlook.com>
2022-11-04 17:26:49 +0800

    added menu view for new scan button

--- whiteboard-scanner/Views/HomeView.swift
+++ whiteboard-scanner/Views/HomeView.swift
@@ -8,11 +8,50 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    // new scan functions
+    func importByCamera() {
+        
+    }
+    func importByPhotos() {
+        
+    }
+    func importByFiles() {
+        
+    }
+    
     var body: some View {
         NavigationView {
             ScrollView(.vertical) {
-                Button {
-                    print("ScanView")
+                Menu {
+                    // by camera
+                    Button {
+                        importByCamera()
+                    } label: {
+                        Image(systemName: "camera")
+                        Text("Camera")
+                    }
+                    // by importing
+                    Menu {
+                        // photos
+                        Button {
+                            importByPhotos()
+                        } label: {
+                            Image(systemName: "photo")
+                            Text("Photos")
+                        }
+                        //files
+                        Button {
+                            importByFiles()
+                        } label: {
+                            Image(systemName: "folder")
+                            Text("Files")
+                        }
+                    } label: {
+                        Image(systemName: "square.and.arrow.down")
+                        Text("Import")
+                    }
+                    
                 } label: {
                     HStack() {
                         Image(systemName: "doc.viewfinder")
