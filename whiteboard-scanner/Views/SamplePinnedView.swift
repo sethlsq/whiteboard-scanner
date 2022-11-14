1a3a68d1353d2dac99d58bc01160dabb712be3d9
Folzo <chrisd1231@gmail.com>
2022-11-14 10:51:25 +0800

    hi

---
+++ whiteboard-scanner/Views/SamplePinnedView.swift
@@ -0,0 +1,82 @@
+//
+//  SamplePinnedView.swift
+//  whiteboard-scanner
+//
+//  Created by T Krobot on 13/11/22.
+//
+
+import SwiftUI
+
+struct SamplePinnedView: View {
+    
+    @AppStorage ("note") var note = ""
+    
+    var body: some View {
+        
+   
+            
+            ZStack {
+                
+                Color.BackgroundColor.edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    
+                    //Potato
+                    ZStack {
+                        
+                        Image("potat0")
+                            .resizable()
+                            .padding()
+                            .scaledToFit()
+                        
+                        Button {
+                            
+                            
+                            
+                        } label: {
+                            
+                            Image(systemName: "pencil")
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(.blue)
+                                .frame(maxWidth: 40, maxHeight: 40)
+                                .cornerRadius(70)
+                                .font(.system(size:30))
+                                .offset(x:145, y:97)
+                            
+                            
+                            
+                        }
+                        
+                    }
+                        
+                    Spacer()
+                    
+                    // Description TextField
+                    VStack {
+                        
+                        TextField("Description", text:$note, axis: .vertical)
+                            .padding()
+                            .background(.white)
+                            .cornerRadius(10)
+                        
+                        
+                        Spacer()
+                        
+                    }
+                    .padding()
+                }
+                
+            }.navigationTitle("Potato")
+            
+            
+        
+        
+    }
+}
+
+struct SamplePinnedView_Previews: PreviewProvider {
+    static var previews: some View {
+        SamplePinnedView()
+    }
+}
